49d5f182af46eb110489b9401014eea8fd0ea483
bryce_nagaj <bnagaj@bgsu.edu>
2022-05-03 22:13:36 -0400

    Add a button to retrieve the user's text input

--- Kivy_Tutorial/main.py
+++ Kivy_Tutorial/main.py
@@ -2,28 +2,47 @@ from kivy.app import App  # import kivy components and libraries from kivy
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 
 class MyGrid(GridLayout):
-    def __init__(self, **kwargs):               # initialization method (kind of like a constructor
+    # initialization method (kind of like a constructor)
+    def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)  # initializes the GridLayout object's constructor
-        self.cols = 2                           # dictates # of columns in the grid
+        self.cols = 1
 
-        # Text input into the grid for a first name
+        self.inside = GridLayout()  # creates a grid inside of another grid to show the submit button at the bottom center
+        self.inside.cols = 2
+
+        # text input into the grid for a first name
         # for each of these text inputs, we add a label, a textbox, then add them to the screen
-        self.add_widget(Label(text="First Name: "))
+        self.inside.add_widget(Label(text="First Name: "))
         self.firstName = TextInput(multiline=False)
-        self.add_widget(self.firstName)
+        self.inside.add_widget(self.firstName)
 
         # text input into the grid for a last name
-        self.add_widget(Label(text="Last Name: "))
+        self.inside.add_widget(Label(text="Last Name: "))
         self.lastName = TextInput(multiline=False)
-        self.add_widget(self.lastName)
+        self.inside.add_widget(self.lastName)
 
         # test input into the grid for an email address
-        self.add_widget(Label(text="Email: "))
+        self.inside.add_widget(Label(text="Email: "))
         self.email = TextInput(multiline=False)
-        self.add_widget(self.email)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
+
+        self.submit = Button(text="Submit", font_size=40)
+        self.submit.bind(on_press=self.pressed)
+        self.add_widget(self.submit)
+
+    # on press event for the submit button
+    def pressed(self, instance):
+        print("| First: ", self.firstName.text, "| Last: ", self.lastName.text, "| Email: ", self.email.text, "|") # print the user's input to the console
+
+        self.firstName.text = ""  # clear the user's input
+        self.lastName.text = ""
+        self.email.text = ""
 
 
 # contains the build method that App utilizes when initialized
@@ -32,5 +51,5 @@ class MyApp(App):
         return MyGrid()  # instantiating the MyGrid object's constructor here
 
 
-if __name__ == "__main__": # tells the python interpreter that this is the main program
+if __name__ == "__main__":  # tells the python interpreter that this is the main program
     MyApp().run()
