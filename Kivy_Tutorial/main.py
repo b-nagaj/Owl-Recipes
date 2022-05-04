7b6fec1683ce4d07516877ca4abca8f8893da8b6
bryce_nagaj <bnagaj@bgsu.edu>
2022-05-04 13:10:42 -0400

    Add a popup window

--- Kivy_Tutorial/main.py
+++ Kivy_Tutorial/main.py
@@ -1,31 +1,32 @@
 from kivy.app import App  # import kivy components and libraries from kivy
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
 
+# event handler for clicking the button on the main page
+class Widgets(Widget):
+    def btn(self):
+        show_popup()
 
-# login page
-class MainWindow(Screen):
+# basic popup window
+class P(FloatLayout):
     pass
 
 
-# page after successful login
-class SecondWindow(Screen):
-    pass
-
-
-# manage transitions between the windows
-class WindowManager(ScreenManager):
-    pass
-
-
-kv = Builder.load_file("my.kv")  # process the specified .kv file (doesn't have to match typical naming convention)
-
-
-# # contains the build method that App utilizes when initialized
-class MyMainApp(App):
+# contains the build method that App utilizes when initialized
+class MyApp(App):
     def build(self):
-        return kv
+        return Widgets()
+
+
+# function to open an overlay of the popup window on the main screen
+def show_popup():
+    show = P()
+
+    popupWindow = Popup(title="New Popup", content=show, size_hint=(None, None), size=(400, 400))  # define a new popup window and initialize it with desired parameters
+
+    popupWindow.open()
 
 
 if __name__ == "__main__":  # tells the python interpreter that this is the main program
-    MyMainApp().run()
+    MyApp().run()
